d345d68c430c67cd29c46096df2a84fd2425d342
Reff20 <fernandoperes.ctt@gmail.com>
2021-06-30 09:25:36 -0300

    release 1.1.0

---
+++ client/src/pages/tracks.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Layout } from '../components';
+import { useQuery, gql } from '@apollo/client';
+import TrackCard from '../containers/track-card';
+import QueryResult from '../components/query-result';
+
+const TRACKS = gql`
+  query Query {
+  tracksForHome {
+    id
+    title
+    author {
+      id
+      name
+      photo
+    }
+    thumbnail
+    length
+    modulesCount
+  }
+}
+`;
+
+const Tracks = () => {
+  const { loading, error, data } = useQuery(TRACKS);
+
+  return <Layout grid>
+    <QueryResult error={error} loading={loading} data={data}>
+      {data?.tracksForHome?.map((track) => (
+        <TrackCard key={track.id} track={track} />
+      ))}
+    </QueryResult>
+  </Layout>;
+};
+
+export default Tracks;
